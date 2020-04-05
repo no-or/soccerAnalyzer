d6d7ce7e86177a1e06fcb94b3298050a21ff780b
Kyle Willis <kyle.c.willis93@gmail.com>
2020-04-05 09:16:23 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import Button from "@paprika/button";
 import GameModal from "./components/gameModal/gameModal";
 import ResultTable from "./components/resultTable/resultTable";
 const { ipcRenderer } = require("electron");
-function twoDigits(d) {
-  if (0 <= d && d < 10) return "0" + d.toString();
-  if (-10 < d && d < 0) return "-0" + (-1 * d).toString();
-  return d.toString();
-}
-Date.prototype.toMysqlFormat = function() {
-  return (
-    this.getUTCFullYear() +
-    "-" +
-    twoDigits(1 + this.getUTCMonth()) +
-    "-" +
-    twoDigits(this.getUTCDate()) +
-    " " +
-    twoDigits(this.getUTCHours()) +
-    ":" +
-    twoDigits(this.getUTCMinutes()) +
-    ":" +
-    twoDigits(this.getUTCSeconds())
-  );
-};
+
 function App() {
   const [games, setGames] = React.useState([]);
   const [leagues, setLeagues] = React.useState([]);
@@ -61,6 +42,10 @@ function App() {
     ipcRenderer.send("getClubs");
     ipcRenderer.send("getReferees");
   }, []);
+
+  const onDelete = id => {
+    ipcRenderer.send("");
+  };
 
   const [isOpen, setIsOpen] = React.useState(false);
   const toggle = () => {
