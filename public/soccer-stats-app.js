d49842cbf4c17855a4f2846d0ae1e4e13f911d39
Arjun Dhadda <arjundhadda236@gmail.com>
2020-04-05 21:16:36 -0700

    exporting division query

--- public/soccer-stats-app.js
+++ public/soccer-stats-app.js
@@ -540,6 +540,7 @@ module.exports.getClubLocations = getClubLocations;
 module.exports.getClubs = getClubs;
 module.exports.getAvgGoalsPerPlayerPerClub = getAvgGoalsPerPlayerPerClub;
 module.exports.getNumGamesPerClub = getNumGamesPerClub;
+module.exports.getClubsThatPlayedInAllLeagueLocations = getClubsThatPlayedInAllLeagueLocations;
 // player functions
 module.exports.getPlayers = getPlayers;
 module.exports.getPlayerInjuries = getPlayerInjuries;
